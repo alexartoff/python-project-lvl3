2332d2433d9acfd6e959248b7b8ebc4679e87201
alexandertarakanov <alex.artoff@gmail.com>
2022-08-08 11:01:15 +0500

    change back exit code value =)

--- page_loader/scripts/loader.py
+++ page_loader/scripts/loader.py
@@ -16,13 +16,13 @@ def main():
         files_path = download(args.url_adress, args.output)
         logging.info(f'html file downloaded and modified "{files_path}"')
         logging.info('done. exit')
-        sys.exit(0)
+        sys.exit()
     except Exception as err:
         logging.error(f'oops! {err}. Stop running...')
         sys.exit(1)
     except BaseException as err:
         logging.error(f'CLI error! Stop running... /// error code: {err}')
-        sys.exit(0)
+        sys.exit(err)
 
 
 if __name__ == "__main__":
